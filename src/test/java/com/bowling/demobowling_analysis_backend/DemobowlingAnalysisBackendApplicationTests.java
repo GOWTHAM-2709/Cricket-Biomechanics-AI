86102491d1f6ea27fa9a698793bd985fd4b19026
GOWTHAM-2709 <gowthamchandrasekaran27@gmail.com>
2026-07-29 12:05:32 +0530

    Fix SpringBootTest configuration

--- src/test/java/com/bowling/demobowling_analysis_backend/DemobowlingAnalysisBackendApplicationTests.java
+++ src/test/java/com/bowling/demobowling_analysis_backend/DemobowlingAnalysisBackendApplicationTests.java
@@ -1,13 +1,13 @@
 package com.bowling.demobowling_analysis_backend;
 
+import com.bowling.demobowlinganalysisbackend.DemobowlingAnalysisBackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemobowlingAnalysisBackendApplication.class)
 class DemobowlingAnalysisBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
